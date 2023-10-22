e432efa84395932c3334792ea22b901081b2035f
Jie Fan <jxiangfan@gmail.com>
2023-10-23 10:41:13 +1100

    fitbit monitoring heart rate

--- app/index.js
+++ app/index.js
@@ -1,1 +1,10 @@
-console.log('Hello world!');
+import { HeartRateSensor } from "heart-rate";
+import { me as appbit } from "appbit";
+
+if (HeartRateSensor && appbit.permissions.granted("access_heart_rate")) {
+  const hrm = new HeartRateSensor({ frequency: 1 });
+  hrm.addEventListener("reading", () => {
+    console.log(`Current heart rate: ${hrm.heartRate}`);
+  });
+  hrm.start();
+}
