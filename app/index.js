a0662c3674abed07ca2dd7238af41b51acceeba0
Jie Fan <jxiangfan@gmail.com>
2023-10-31 23:16:54 +1100

    heart rate to show on the display

--- app/index.js
+++ app/index.js
@@ -1,10 +1,18 @@
 import { HeartRateSensor } from "heart-rate";
 import { me as appbit } from "appbit";
+let document = require("document");
+
+// Fetch UI element of heart rate
+let heartLabel = document.getElementById("heart");
+
+// Initialize the UI with some values
+heartLabel.text = "Current heart rate: --";
 
 if (HeartRateSensor && appbit.permissions.granted("access_heart_rate")) {
-  const hrm = new HeartRateSensor({ frequency: 1 });
-  hrm.addEventListener("reading", () => {
-    console.log(`Current heart rate: ${hrm.heartRate}`);
+  const hrSensor = new HeartRateSensor({ frequency: 1 });
+  hrSensor.addEventListener("reading", () => {
+    console.log(`Current heart rate: ${hrSensor.heartRate}`);
+    heartLabel.text = `Current heart rate: ${hrSensor.heartRate}`;
   });
-  hrm.start();
+  hrSensor.start();
 }
